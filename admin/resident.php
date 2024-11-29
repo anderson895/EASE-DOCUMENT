a769cccba945848855d77315eae9f793444cf9d3
JARP <andersonandy046@gmail.com>
2024-11-29 10:41:12 +0800

    fetch resident

--- admin/resident.php
+++ admin/resident.php
@@ -3,7 +3,7 @@ include "components/header.php";
 ?>
 
 <div class="flex justify-between items-center bg-white p-4 mb-6 rounded-md shadow-md">
-    <h2 class="text-lg font-semibold text-gray-700">Customer</h2>
+    <h2 class="text-lg font-semibold text-gray-700">Resident List</h2>
     <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-lg font-bold text-white">
         <?php
         echo substr(ucfirst($_SESSION['user_type']), 0, 1);
@@ -13,17 +13,20 @@ include "components/header.php";
 
 <!-- Card for Table -->
 <div class="bg-white rounded-lg shadow-lg p-6">
-    <h3 class="text-xl font-semibold text-gray-700 mb-4">Resident List</h3>
    
-    <!-- Search Input -->
-    <div class="mb-4 flex items-center space-x-4">
-        <button id="adduserButton" class="bg-blue-500 text-white py-1 px-3 text-xs rounded-sm flex items-center hover:bg-blue-600 transition duration-300">
-            <span class="material-icons mr-1 text-sm">person_add</span>
-            Add Resident
-        </button>
+   <!-- Add Resident Button -->
+<div class="mb-4 flex items-center space-x-4">
+    <button id="addResidentButton" class="bg-blue-500 text-white py-1 px-3 text-xs rounded-md flex items-center hover:bg-blue-600 transition duration-300">
+        <span class="material-icons mr-1 text-sm">person_add</span>
+        Add Resident
+    </button>
+</div>
 
-        <input type="text" id="searchInput" class="p-2 border rounded-sm text-xs" placeholder="Search resident...">
-    </div>
+<!-- Search Input -->
+<div class="mb-4">
+    <label for="searchInput" class="block text-xs text-gray-700 mb-1">Search</label>
+    <input type="text" id="searchInput" class="p-2 border rounded-sm text-xs w-60" placeholder="Search resident...">
+</div>
 
 
 

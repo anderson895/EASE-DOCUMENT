8c65a940c3051b43e6c7298b52eb50a8ef29c27c
JARP <andersonandy046@gmail.com>
2024-12-09 13:51:15 +0800

    delete modal

--- admin/backend/end-points/resident_list.php
+++ admin/backend/end-points/resident_list.php
@@ -95,33 +95,3 @@ if ($fetch_all_resident): ?>
 
 
 
-
-<!-- Modal -->
-<div id="deleteConfirmationModal" class="fixed z-10 inset-0 overflow-y-auto" style="display:none;" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-        
-        <div id="DeleteResidentloadingSpinner" style="display:none;">
-                <div class=" absolute inset-0 bg-white bg-opacity-75 flex items-center justify-center">
-                <div class="w-10 h-10 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin"></div>
-                </div>
-            </div>
-        <!-- Modal Content -->
-        <form id="frmDeleteResident" class="space-y-6">
-
-            <input type="text" id="TargetdelResidentId" name="TargetdelResidentId">
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Confirm Deletion</h3>
-                    <div class="mt-2">
-                        <p class="text-sm text-gray-500">Are you sure you want to Remove this resident?</p>
-                    </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button id="confirmDeleteResident" type="button" class="bg-red-500 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400">Delete</button>
-                    <button type="button" class="cancelDeleteResident bg-gray-300 text-gray-700 px-4 py-2 rounded-md text-sm font-medium hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 sm:mr-2">Cancel</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>

26fb7ae84482ad55bf00ebf56debcc3e3c47c522
JARP <andersonandy046@gmail.com>
2024-12-09 23:24:37 +0800

    bugs

--- admin/components/header.php
+++ admin/components/header.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['user_id'])) {
   <link rel="icon" type="image/png" href="../assets/logo.jpeg">
   
   <script src="https://cdn.tailwindcss.com"></script>
-  <!-- <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script> -->
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/css/alertify.css" integrity="sha512-MpdEaY2YQ3EokN6lCD6bnWMl5Gwk7RjBbpKLovlrH6X+DRokrPRAF3zQJl1hZUiLXfo2e9MrOt+udOnHCAmi5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/alertify.min.js" integrity="sha512-JnjG+Wt53GspUQXQhc+c4j8SBERsgJAoHeehagKHlxQN+MtCCmFDghX9/AcbkkNRZptyZU4zC8utK59M5L45Iw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
